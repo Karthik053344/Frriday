dd7b2d1e6cd5a3d5ea324cd4b7129f187fe1aa62
Karthik053344 <138372026+Karthik053344@users.noreply.github.com>
2026-09-19 19:58:34 +0000

    Delete shopify-config.js

--- shopify-config.js
+++
@@ -1,6 +0,0 @@
-window.FRRIDAY_SHOPIFY = Object.freeze({
-  store: 'pxhdjy-sj.myshopify.com',
-  apiVersion: '2026-07',
-  storefrontToken: '04f26b87a99e673c20b49be5baf14847',
-  endpoint: 'https://pxhdjy-sj.myshopify.com/api/2026-07/graphql.json'
-});
